e33d9618604eb66f1c8680addd05393a28212463
Adrian Nuta <adriannuta@gmail.com>
2016-09-19 15:53:46 +0300

    update menus

--- template/footer.php
+++ template/footer.php
@@ -5,7 +5,7 @@
 	<footer>
 		<hr>
 	
-        <p>Copyright &copy; 2013-2014, Sphinx Technologies Inc.</p>
+        <p>Copyright &copy; 2013-2016, Sphinx Technologies Inc.</p>
      </footer>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
